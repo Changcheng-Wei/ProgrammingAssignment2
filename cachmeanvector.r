3fa3058b840dece025f7dc40be3346fb875a3762
Changcheng-Wei <weichangcheng@sina.com>
2025-05-06 13:58:20 +0800

    Update cachmeanvector.r
    Add explanation for each function and step

--- cachmeanvector.r
+++ cachmeanvector.r
@@ -1,47 +1,52 @@
 makeVector <- function(x = numeric()) {
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
+ #  This function creates a special "vector" object that can cache its mean. It contains a list of functions to set and get the value of the vector and to set and get the mean of the vector. The function returns a list of these functions. The function is used in conjunction with the cachemean function to calculate the mean of the vector and cache the result for future use. The function is used to avoid recalculating the mean of the vector if it has already been calculated and cached.The function is used to improve the performance of the program by avoiding unnecessary calculations.The function is used to demonstrate the use of lexical scoping and the use of functions as first-class objects in R.The function is used to demonstrate the use of closures in R and the use of the <<- operator to assign values to variables 
+  set <- function(y) { # set the value of the vector
+    x <<- y # assign the value of y to x
+    m <<- NULL # set the mean to NULL
   }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  get <- function() x # get the value of the vector
+  setmean <- function(mean) m <<- mean # set the mean of the vector
+  getmean <- function() m # get the mean of the vector
   list(set = set, get = get,
        setmean = setmean,
-       getmean = getmean)
+       getmean = getmean)# return the list of functions
 }
 cachemean <- function(x, ...) {
-  m <- x$getmean()
+  # This function creates a special "vector" object that can cache its mean.
+
+  m <- x$getmean() # This function checks if the mean has already been calculated.
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
-  }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
+  }# If the mean is not already calculated, it calculates the mean of the data.
+  data <- x$get()# This function retrieves the data from the special "vector" object.
+  m <- mean(data, ...)# This function calculates the mean of the data.
+  x$setmean(m)# This function sets the mean in the special "vector" object.
+  m# This function returns the mean.
 }
-makecacheMatrix <- function(x = matrix()) {
-  m <- NULL
+
+makecacheMatrix <- function(x = matrix()) { # This function creates a special "matrix" object that can cache its inverse.
+  m <- NULL # Initialize the inverse property
   set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) m <<- inverse
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
-}
-cacheSolve <- function(x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
-}
+    x <<- y # This function sets the value of the matrix
+    m <<- NULL # Reset the inverse property
+  }# This function sets the value of the matrix
+  get <- function() x # This function gets the value of the matrix
+  setinverse <- function(inverse) m <<- inverse # This function sets the inverse of the matrix
+  getinverse <- function() m # This function gets the inverse of the matrix
+  list(set = set, get = get, # This function gets the value of the matrix
+       setinverse = setinverse, 
+       getinverse = getinverse) # This function gets the inverse of the matrix
+} # This function creates a special "matrix" object that can cache its inverse.
+
+cacheSolve <- function(x, ...) { # This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+  m <- x$getinverse() # This function retrieves the cached inverse matrix if it exists.
+  if(!is.null(m)) { # If the cached inverse matrix is not NULL, it means we have already computed the inverse.
+    message("getting cached data") # This message indicates that we are using the cached data.
+    return(m) # Return the cached inverse matrix.
+  } # If the cached inverse matrix is NULL, we need to compute it.
+  data <- x$get() # This function retrieves the original matrix.
+  m <- solve(data, ...) # This function computes the inverse of the original matrix.
+  x$setinverse(m) # This function caches the computed inverse matrix.
+  m # Return the computed inverse matrix.
+} # End of the function
